ae12c37ccfd423ab7ff4fc7bdb1aa34c71dc0834
Janani M <jananimurthi09@gmail.com>
2020-07-29 18:21:43 +0530

    'changed'

--- site/assignment4/script.js
+++
@@ -1,9 +0,0 @@
-var arrayName=["jumboo","jack","kiran"];
-for (var name in arrayName){
-	if(arrayName[name].charAt(0).toUpperCase() == 'J'){
-		console.log(`Hello ${arrayName[name].charAt(0).toUpperCase()}${arrayName[name]}`);
-	}
-	else{
-	console.log('Hello '+arrayName[name]);
-    }
-}
