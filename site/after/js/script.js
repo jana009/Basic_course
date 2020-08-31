4c97bf79a87dfa53025d8c03c9de6d5679ee126d
Janani M <jananimurthi09@gmail.com>
2020-08-31 19:32:34 +0530

    myassignment5

---
+++ site/after/js/script.js
@@ -0,0 +1,8 @@
+// $(function (){
+// 	$(".navbar-nav>li>a").focusout(function(){
+// 		var screen_width=window.innerwidth;
+// 		if(screen_width<767){
+// 			$(".navbar-collapse").collapse('hide');
+// 		}
+// 	});
+// });
--- site/assignment4/js/script.js
+++ site/assignment4/js/script.js
@@ -1,10 +1,35 @@
 
-var arrayName=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
-for (var name in arrayName){
-       if(arrayName[name].charAt(0).toUpperCase() == 'J'){
-       	byespeaker.bye(arrayName[name]);
-       }
-       else{
-       	hellospeaker.bye(arrayName[name]);
-       }
-}
+// var arrayName=["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
+// for (var name in arrayName){
+//        if(arrayName[name].charAt(0).toUpperCase() == 'J'){
+//        	byespeaker.bye(arrayName[name]);
+//        }
+//        else{
+//        	hellospeaker.bye(arrayName[name]);
+//        }
+// }
+// console.log(document.getElementById('textbox'))
+// document.addEventListener('DOMContentLoaded',
+// 	function(event){
+// function hello(){
+// 	this.textContent='submitted'
+// 	var greeting='Hello '+document.getElementById('textbox').value+'!'
+// 	document.getElementById('content').style ='text-align :center'
+// 	document.getElementById('content').innerHTML=greeting;
+// 	//var title =document.querySelector('h1').textContent;
+// 	//title += ' Interesting';
+// 	//document.querySelector('h1').textContent =title;
+
+// }
+// //document.getElementById('name').onclick=hello;
+// document.querySelector('button').addEventListener('click',hello)
+// document.querySelector('body').addEventListener('mousemove',function(event){
+// 	console.log(event);
+// 	console.log(this);
+// 	// for(let i=0;i<3;i++){
+// 	// console.log(event.clientX);}
+// })
+// }
+// );
+// Ajax call
+
